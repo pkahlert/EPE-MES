38bc5bc8c202f28629d3b89b2f2561c6780d579c
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2015-11-04 11:08:10 +0100

    workspace bumms

---
+++ 02_Software/01_c2000/Example_F2802xLaunchPadDemo/Flash/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_examples/C2kLaunchPadDemo/../../f2802x_common/cmd/F28027.cmd" -l"rts2800_ml.lib" -l"IQmath.lib"
+
