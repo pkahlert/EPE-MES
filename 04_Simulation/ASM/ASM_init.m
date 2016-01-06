05dceb52fb4006abb41e51b2bcd90a63252ccb79
7be <nicsiebenborn@gmail.com>
2016-01-06 13:24:51 +0100

    Gutes Modell gefunden

--- 04_Simulation/ASM/ASM_init.m
+++ 04_Simulation/ASM/ASM_init.m
@@ -1,21 +1,21 @@
 
 %% Allgemeine Parameter
 wb  = 2*pi*50;        % Signalfrequenz
-fHz = 2000; 
+fHz = 50; 
 Vdc = 220; %/sqrt(3)
 
 %% Maschinen Parameter
-p  = 4;
-J  = 0.028;
+p  = 2;
+J  = 0.0006;
 Lr = 0.04e-3;
 Ls = 0.04e-3;
 Lm = 0.42e-3;
-Rs = 10;
-Rr = 6.3;
+Rs = 30;
+Rr = 30;
 Xm  = wb*Lm; % 
 Xls = wb*Ls; % 
 Xlr = wb*Lr; % 
-Xmlstern = 1/(1/Xls+1/Xm+1/Xlr);
+Xmlstern = 1/((1/Xls)+(1/Xm)+(1/Xlr));
 
 %% Gewollte Parameter
 
@@ -23,4 +23,4 @@ we = 4*pi*fHz/p;   % Statorfrequenz
 
 %% Last Parameter
 
-Tl = 0;
+Tl = 5;
