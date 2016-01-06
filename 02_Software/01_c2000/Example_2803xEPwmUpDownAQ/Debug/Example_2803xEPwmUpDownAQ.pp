245e25af94c12c419cf70bf79d420f125a7861a5
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2016-01-06 09:18:03 +0100

    alles da

---
+++ 02_Software/01_c2000/Example_2803xEPwmUpDownAQ/Debug/Example_2803xEPwmUpDownAQ.pp
@@ -0,0 +1,65 @@
+# FIXED
+
+Example_2803xEPwmUpDownAQ.obj: ../Example_2803xEPwmUpDownAQ.c
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP28x_Project.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_Cla_typedefs.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Device.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Adc.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_BootVars.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_DevEmu.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Cla.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Comp.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_CpuTimers.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_ECan.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_ECap.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_EPwm.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_EQep.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Gpio.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_HRCap.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_I2c.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Lin.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_NmiIntrupt.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_PieCtrl.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_PieVect.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Spi.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Sci.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_SysCtrl.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_XIntrupt.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_Examples.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_GlobalPrototypes.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_EPwm_defines.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_I2c_defines.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_Cla_defines.h
+Example_2803xEPwmUpDownAQ.obj: C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_DefaultISR.h
+
+../Example_2803xEPwmUpDownAQ.c: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP28x_Project.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_Cla_typedefs.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Device.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Adc.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_BootVars.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_DevEmu.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Cla.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Comp.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_CpuTimers.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_ECan.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_ECap.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_EPwm.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_EQep.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Gpio.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_HRCap.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_I2c.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Lin.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_NmiIntrupt.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_PieCtrl.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_PieVect.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Spi.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_Sci.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_SysCtrl.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/include/DSP2803x_XIntrupt.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_Examples.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_GlobalPrototypes.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_EPwm_defines.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_I2c_defines.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_Cla_defines.h: 
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/include/DSP2803x_DefaultISR.h: 
---
+++ 02_Software/01_c2000/Example_2803xEPwmUpDownAQ/Debug/subdir_vars.mk
@@ -0,0 +1,90 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/cmd/28035_RAM_lnk.cmd \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/cmd/DSP2803x_Headers_nonBIOS.cmd 
+
+ASM_SRCS += \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_CodeStartBranch.asm \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_usDelay.asm 
+
+C_SRCS += \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_CpuTimers.c \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_DefaultIsr.c \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_EPwm.c \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/source/DSP2803x_GlobalVariableDefs.c \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_PieCtrl.c \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_PieVect.c \
+C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_SysCtrl.c \
+../Example_2803xEPwmUpDownAQ.c 
+
+OBJS += \
+./DSP2803x_CodeStartBranch.obj \
+./DSP2803x_CpuTimers.obj \
+./DSP2803x_DefaultIsr.obj \
+./DSP2803x_EPwm.obj \
+./DSP2803x_GlobalVariableDefs.obj \
+./DSP2803x_PieCtrl.obj \
+./DSP2803x_PieVect.obj \
+./DSP2803x_SysCtrl.obj \
+./DSP2803x_usDelay.obj \
+./Example_2803xEPwmUpDownAQ.obj 
+
+ASM_DEPS += \
+./DSP2803x_CodeStartBranch.pp \
+./DSP2803x_usDelay.pp 
+
+C_DEPS += \
+./DSP2803x_CpuTimers.pp \
+./DSP2803x_DefaultIsr.pp \
+./DSP2803x_EPwm.pp \
+./DSP2803x_GlobalVariableDefs.pp \
+./DSP2803x_PieCtrl.pp \
+./DSP2803x_PieVect.pp \
+./DSP2803x_SysCtrl.pp \
+./Example_2803xEPwmUpDownAQ.pp 
+
+C_DEPS__QUOTED += \
+"DSP2803x_CpuTimers.pp" \
+"DSP2803x_DefaultIsr.pp" \
+"DSP2803x_EPwm.pp" \
+"DSP2803x_GlobalVariableDefs.pp" \
+"DSP2803x_PieCtrl.pp" \
+"DSP2803x_PieVect.pp" \
+"DSP2803x_SysCtrl.pp" \
+"Example_2803xEPwmUpDownAQ.pp" 
+
+OBJS__QUOTED += \
+"DSP2803x_CodeStartBranch.obj" \
+"DSP2803x_CpuTimers.obj" \
+"DSP2803x_DefaultIsr.obj" \
+"DSP2803x_EPwm.obj" \
+"DSP2803x_GlobalVariableDefs.obj" \
+"DSP2803x_PieCtrl.obj" \
+"DSP2803x_PieVect.obj" \
+"DSP2803x_SysCtrl.obj" \
+"DSP2803x_usDelay.obj" \
+"Example_2803xEPwmUpDownAQ.obj" 
+
+ASM_DEPS__QUOTED += \
+"DSP2803x_CodeStartBranch.pp" \
+"DSP2803x_usDelay.pp" 
+
+ASM_SRCS__QUOTED += \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_CodeStartBranch.asm" \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_usDelay.asm" 
+
+C_SRCS__QUOTED += \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_CpuTimers.c" \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_DefaultIsr.c" \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_EPwm.c" \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_headers/source/DSP2803x_GlobalVariableDefs.c" \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_PieCtrl.c" \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_PieVect.c" \
+"C:/Beuth/EPE-MES/02_Software/01_c2000/DSP2803x_common/source/DSP2803x_SysCtrl.c" \
+"../Example_2803xEPwmUpDownAQ.c" 
+
+
